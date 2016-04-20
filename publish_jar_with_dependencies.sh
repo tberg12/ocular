53fd939d7931627f578f308dfa30f8b062cb5d2e
dhg <dhgarrette@gmail.com>
2016-04-20 09:08:31 -0700

    explicitly specify 0.3-SNAPSHOT as the version in publish_jar_with_dependencies.sh

--- publish_jar_with_dependencies.sh
+++ publish_jar_with_dependencies.sh
@@ -3,5 +3,5 @@
 set -e
 
 ./make_jar.sh
-scp ocular-*-with_dependencies.jar k:public_html/maven-repository/snapshots/edu/berkeley/cs/nlp/ocular/0.3-SNAPSHOT/
+scp ocular-0.3-SNAPSHOT-with_dependencies.jar k:public_html/maven-repository/snapshots/edu/berkeley/cs/nlp/ocular/0.3-SNAPSHOT/
 
