3f23bea9a8d194c9eeafc25664dbbab915d6906b
Dan Garrette <dhgarrette@gmail.com>
2017-01-24 15:05:02 -0500

    Added main method to Straightener

--- src/main/java/edu/berkeley/cs/nlp/ocular/preprocessing/Straightener.java
+++ src/main/java/edu/berkeley/cs/nlp/ocular/preprocessing/Straightener.java
@@ -1,8 +1,13 @@
 package edu.berkeley.cs.nlp.ocular.preprocessing;
 
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.List;
 
 import edu.berkeley.cs.nlp.ocular.image.ImageUtils;
+import edu.berkeley.cs.nlp.ocular.image.ImageUtils.ConnectedComponentProcessor;
+import tberg.murphy.fileio.f;
 
 /**
  * @author Taylor Berg-Kirkpatrick (tberg@eecs.berkeley.edu)
@@ -44,4 +49,46 @@ public class Straightener {
 		return totalVar / (levels[0].length-1);
 	}
 	
+	public static void main(String[] args) {
+		String path = args[0];
+		double binarizeThresh = 0.1;
+		if (args.length > 1) {
+			binarizeThresh = Double.parseDouble(args[1]);
+		}
+		File dir = new File(path);
+		String[] names = dir.list(new FilenameFilter() {
+			public boolean accept(File dir, String name) {
+				return name.endsWith(".png") || name.endsWith(".jpg");
+			}
+		});
+		File straightDir = new File(path + "/straight");
+		straightDir.mkdirs();
+		for (String name : names) {
+			double[][] levels = ImageUtils.getLevels(f.readImage(path+"/"+name));
+			ConnectedComponentProcessor ccprocBig = new ConnectedComponentProcessor() {
+				public void process(double[][] levels, List<int[]> connectedComponent) {
+					if (connectedComponent.size() > 1000) {
+						for (int[] pixel : connectedComponent) {
+							levels[pixel[0]][pixel[1]] = 255.0;
+						}
+					}
+				}
+			};
+			ImageUtils.processConnectedComponents(levels, 50.0, ccprocBig);
+			Binarizer.binarizeGlobal(binarizeThresh, levels);
+			ConnectedComponentProcessor ccprocSmall = new ConnectedComponentProcessor() {
+				public void process(double[][] levels, List<int[]> connectedComponent) {
+					if (connectedComponent.size() < 20 || connectedComponent.size() > 500) {
+						for (int[] pixel : connectedComponent) {
+							levels[pixel[0]][pixel[1]] = 255.0;
+						}
+					}
+				}
+			};
+			ImageUtils.processConnectedComponents(levels, 127.0, ccprocSmall);
+			double[][] rotLevels = Straightener.straighten(levels);
+			String baseName = (name.lastIndexOf('.') == -1) ? name : name.substring(0, name.lastIndexOf('.'));
+			f.writeImage(straightDir.getAbsolutePath() +"/"+ baseName + ".png", ImageUtils.makeImage(rotLevels));
+		}
+	}
 }
