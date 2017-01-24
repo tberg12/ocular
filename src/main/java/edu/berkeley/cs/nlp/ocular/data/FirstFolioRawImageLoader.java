d1893e6cbb34e2e1cc35f4ab5fd8517ffffccb99
Dan Garrette <dhgarrette@gmail.com>
2017-01-24 14:41:33 -0500

    added FirstFolioRawImageLoader

---
+++ src/main/java/edu/berkeley/cs/nlp/ocular/data/FirstFolioRawImageLoader.java
@@ -0,0 +1,114 @@
+package edu.berkeley.cs.nlp.ocular.data;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.Arrays;
+import java.util.List;
+
+import edu.berkeley.cs.nlp.ocular.image.ImageUtils;
+import edu.berkeley.cs.nlp.ocular.image.ImageUtils.ConnectedComponentProcessor;
+import edu.berkeley.cs.nlp.ocular.image.ImageUtils.PixelType;
+import edu.berkeley.cs.nlp.ocular.main.Main;
+import edu.berkeley.cs.nlp.ocular.preprocessing.Binarizer;
+import edu.berkeley.cs.nlp.ocular.preprocessing.LineExtractor;
+import tberg.murphy.arrays.a;
+import tberg.murphy.fileio.f;
+import tberg.murphy.threading.BetterThreader;
+
+public class FirstFolioRawImageLoader {
+
+	public static class FirstFolioRawImageDocument implements Document {
+		private final String baseName;
+		final PixelType[][][] observations;
+		
+		public FirstFolioRawImageDocument(String inputPath, String baseName, int lineHeight) {
+			this.baseName = baseName;
+			double[][] levels = ImageUtils.getLevels(f.readImage(inputPath+"/"+baseName));
+			ConnectedComponentProcessor ccprocBig = new ConnectedComponentProcessor() {
+				public void process(double[][] levels, List<int[]> connectedComponent) {
+					if (connectedComponent.size() > 1000) {
+						for (int[] pixel : connectedComponent) {
+							levels[pixel[0]][pixel[1]] = 255.0;
+						}
+					}
+				}
+			};
+			ImageUtils.processConnectedComponents(levels, 50.0, ccprocBig);
+			Binarizer.binarizeGlobal(Main.binarizeThreshold, levels);
+			ConnectedComponentProcessor ccprocSmall = new ConnectedComponentProcessor() {
+				public void process(double[][] levels, List<int[]> connectedComponent) {
+					if (connectedComponent.size() < 20 || connectedComponent.size() > 1000) {
+						for (int[] pixel : connectedComponent) {
+							levels[pixel[0]][pixel[1]] = 255.0;
+						}
+					}
+				}
+			};
+			ImageUtils.processConnectedComponents(levels, 127.0, ccprocSmall);
+			
+			int padHeight = 0;
+			double[][] topPadLevels = new double[levels.length][];
+			for (int i=0; i<levels.length; ++i) topPadLevels[i] = a.append(a.add(a.zerosDouble(padHeight), 255.0), levels[i]);
+			
+			List<double[][]> lines = LineExtractor.extractLines(topPadLevels);
+			observations = new PixelType[lines.size()][][];
+			for (int i=0; i<lines.size(); ++i) {
+				if (lineHeight >= 0) {
+					observations[i] = ImageUtils.getPixelTypes(ImageUtils.resampleImage(ImageUtils.makeImage(lines.get(i)), lineHeight));
+				} else {
+					observations[i] = ImageUtils.getPixelTypes(ImageUtils.makeImage(lines.get(i)));
+				}
+			}
+		}
+
+		public PixelType[][][] loadLineImages() {
+			return observations;
+		}
+
+		public String[][] loadDiplomaticTextLines() {
+			return null;
+		}
+		
+		public String[][] loadNormalizedTextLines() {
+			return null;
+		}
+		
+		public List<String> loadNormalizedText() {
+			return null;
+		}
+		
+		public String baseName() {
+			return baseName;
+		}
+	}
+	
+	private final String inputPath;
+	private final int lineHeight;
+	private final int numThreads;
+
+	public FirstFolioRawImageLoader(String inputPath, int lineHeight, int numThreads) {
+		this.inputPath = inputPath;
+		this.lineHeight = lineHeight;
+		this.numThreads = numThreads;
+	}
+	
+	public List<Document> readDataset() {
+		System.out.println("Extracting text line images from dataset..");
+		File dir = new File(inputPath);
+		final String[] dirList = dir.list(new FilenameFilter() {
+			public boolean accept(File dir, String name) {
+				return name.endsWith(".png") || name.endsWith(".jpg");
+			}
+		});
+		final Document[] docs = new Document[dirList.length]; 
+		BetterThreader.Function<Integer,Object> func = new BetterThreader.Function<Integer,Object>(){public void call(Integer i, Object ignore){
+			String baseName = dirList[i];
+			docs[i] = new FirstFolioRawImageDocument(inputPath, baseName, lineHeight);
+		}};
+		BetterThreader<Integer,Object> threader = new BetterThreader<Integer,Object>(func, numThreads);
+		for (int i=0; i<dirList.length; ++i) threader.addFunctionArgument(i);
+		threader.run();
+		return Arrays.asList(docs);
+	}
+
+}
