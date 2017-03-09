04047c5591e76a60eee6a05222f035f3210a2b35
Dan Garrette <dhgarrette@gmail.com>
2017-03-09 10:47:07 -0500

    Always output ALTO format from emop

--- src/main/java/edu/berkeley/cs/nlp/ocular/main/FonttrainTranscribeShared.java
+++ src/main/java/edu/berkeley/cs/nlp/ocular/main/FonttrainTranscribeShared.java
@@ -177,6 +177,7 @@ public abstract class FonttrainTranscribeShared extends LineExtractionOptions {
 		}
 		if (formats.isEmpty()) {
 			formats.add(DIPL);
+			formats.add(ALTO); // Always output ALTO format from emop
 			if (allowGlyphSubstitution)
 				formats.add(NORM);
 		}
