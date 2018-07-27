2e4e23a14def263739ea20a5440c2cfb2abf169e
Taylor Berg-Kirkpatrick <tberg@Taylors-MBP.wv.cc.cmu.edu>
2018-07-27 15:33:30 -0400

    font export fix

--- src/main/java/edu/berkeley/cs/nlp/ocular/main/ExportFont.java
+++ src/main/java/edu/berkeley/cs/nlp/ocular/main/ExportFont.java
@@ -1,30 +1,14 @@
 package edu.berkeley.cs.nlp.ocular.main;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.zip.GZIPInputStream;
-import java.util.zip.GZIPOutputStream;
 
-import edu.berkeley.cs.nlp.ocular.data.textreader.Charset;
 import edu.berkeley.cs.nlp.ocular.font.Font;
-import edu.berkeley.cs.nlp.ocular.image.FontRenderer;
-import edu.berkeley.cs.nlp.ocular.image.ImageUtils.PixelType;
-import edu.berkeley.cs.nlp.ocular.lm.LanguageModel;
 import edu.berkeley.cs.nlp.ocular.model.CharacterTemplate;
 import tberg.murphy.arrays.a;
 import tberg.murphy.fig.Option;
 import tberg.murphy.fileio.f;
-import tberg.murphy.indexer.Indexer;
-import tberg.murphy.threading.BetterThreader;
 
 /**
  * @author Taylor Berg-Kirkpatrick (tberg@eecs.berkeley.edu)
@@ -51,6 +35,9 @@ public class ExportFont extends OcularRunnable {
 	public void run(List<String> commandLineArgs) {
 		Font font = InitializeFont.readFont(inputFontPath);
 		
+		File outDir = new File(outputDirPath);
+		outDir.mkdirs();
+		
 		for (Map.Entry<String,CharacterTemplate> entry : font.charTemplates.entrySet()) {
 			CharacterTemplate template = entry.getValue();
 			int bestWidth = -1;
@@ -62,7 +49,9 @@ public class ExportFont extends OcularRunnable {
 				}
 			}
 			float[][] probs = template.blackProbs(0, 0, bestWidth);
-			f.writeString(outputDirPath+"/"+entry.getValue(), a.toString(probs));
+			String charName = entry.getKey();
+			if (!charName.matches("[a-zA-Z0-9]+")) charName = Integer.toString((int) charName.charAt(0));
+			f.writeString(outputDirPath+"/char_"+entry.getValue(), a.toString(probs));
 		}
 		
 	}
